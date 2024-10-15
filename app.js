5b5cb73d8b5273de5f0cee399298c9c29751f29a
AK1239 <akilkhatri235@gmail.com>
2024-10-15 11:24:12 +0300

    security features

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ require("express-async-errors");
 const express = require("express");
 const app = express();
 
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
 const authRouter = require("./routes/auth");
 const jobsRouter = require("./routes/jobs");
 
@@ -13,8 +18,17 @@ const authenticateUser = require("./middleware/authentication");
 const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+  })
+);
 app.use(express.json());
-// extra packages
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 // routes
 app.use("/api/v1/auth", authRouter);
